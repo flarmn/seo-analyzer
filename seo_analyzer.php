87e8d1d708344cbf8c90b27b3a8e8789ccdaae13
Unknown <flarmn@gmail.com>
2017-04-11 23:31:02 +0300

    Engine + Front end: start

--- seo_analyzer.php
+++ seo_analyzer.php
@@ -1,9 +1,25 @@
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<title>Item details</title>
+</head>
+<body>
+
+<div class = "container">
 <form method = "post" >
 <input type = "textfield" name = "address" placeholder="Input your web-site address">
 <input type = "submit" value = "Check">
 </form>
+
 
 
 <?php
@@ -17,8 +33,16 @@ $robotspath = fopen($checkedAddress . '/robots.txt', 'r');
 // Checking if file exist at place
 if ($robotspath == false){
 echo 'No robots file';
+$robotsExistStatus = "Файл robots.txt отсутствует";
+	$robotsExistRecomendation = "Программист: Создать файл robots.txt и разместить его на сайте.";
+	$robotsStatus = '<p style = "display:inline-block; background:red; color:white;">Ошибка</p>';
+
 } else {
 	echo 'Robots file exist. All ok!';
+	$robotsExistStatus = "Файл robots.txt присутствует";
+	$robotsExistRecomendation = "Доработки не требуются";
+	$robotsStatus = '<p style = "display:inline-block; height:100%; width:100%; background:green; color:white;">Ok</p>';
+
 }
 
 // Getting server answer
@@ -49,16 +73,150 @@ $termsCount = substr_count($robotssearch, $searchTerm);
 
 if (substr_count($robotssearch, $searchTerm)>0){
     echo "<br/>Найден! Искомое слово встречаеться " . $termsCount . ' раз';
+
+    $hostExistStatus = "Директива Host указана";
+	$hostExistRecomendation = "Доработки не требуются";
+	$hostStatus = '<p style = "display:inline-block; height:100%; width:100%; background:green; color:white;">Ok</p>';
 }
 else{
     echo "<br/>Не найден!";
+    $hostExistStatus = "В файле robots.txt не указана директива Host";
+	$hostExistRecomendation = "Программист: Для того, чтобы поисковые системы знали, какая версия сайта является основных зеркалом, необходимо прописать адрес основного зеркала в директиве Host. В данный момент это не прописано. Необходимо добавить в файл robots.txt директиву Host. Директива Host задётся в файле 1 раз, после всех правил.";
+$hostStatus = '<p style = "display:inline-block; height:100%; width:100%; background:red; color:white;">Oшибка</p>';
 } 
 
-/*
-echo '<br/>';
-print_r($remoteFileMeta);
-*/
+
+
 
 //END 
 ?>
+
+
+
+<h1 class = "text-center">Результаты анализа сайта:</h1>
+<h3 class = "text-center"><?php echo $checkedAddress; ?></h3>
+
+<table class="table table-bordered">
+
+<thead>
+
+<tr>
+	<th>
+	№
+	</th>
+	<th>
+	Название проверки
+	</th>
+	<th>
+	Статус
+	</th>
+	<th>
+	Текущее состояние
+	</th>
+</tr>
+</thead>
+
+<tbody>
+<tr>
+	<th>
+	№
+	</th>
+	<th>
+	Проверка наличия файла robots.txt
+	</th>
+	<th>
+	<?php echo $robotsStatus; ?>
+	</th>
+	<th>
+	<?php echo 'Состояние: ' . $robotsExistStatus . "<br/>"; ?>
+	<hr>
+	<?php echo 'Рекомендации: ' . $robotsExistRecomendation . "<br/>"; ?>
+	</th>
+</tr>
+
+<tr >
+	<th>
+	№325
+	</th>
+	<th>
+	Проверка указания директивы Host
+	</th>
+	<th>
+	<?php echo $hostStatus; ?>
+	</th>
+	<th style = "width: 45%">
+	<?php echo 'Состояние: ' . $hostExistStatus . "<br/>"; ?>
+	<hr>
+	<?php echo 'Рекомендации: ' . $hostExistRecomendation . "<br/>"; ?>
+	</th>
+</tr>
+
+
+<tr>
+	<th>
+	№
+	</th>
+	<th>
+	Проверка количества директив Host, прописанных в файле
+	</th>
+	<th>
+	Статус
+	</th>
+	<th>
+	Текущее состояние
+	</th>
+</tr>
+
+
+<tr>
+	<th>
+	№
+	</th>
+	<th>
+	Проверка размера файла robots.txt
+	</th>
+	<th>
+	Статус
+	</th>
+	<th>
+	Текущее состояние
+	</th>
+</tr>
+
+
+<tr>
+	<th>
+	№
+	</th>
+	<th>
+	Проверка кода ответа сервера для файла robots.txt
+	</th>
+	<th>
+	Статус
+	</th>
+	<th>
+	Текущее состояние
+	</th>
+</tr>
+
+
+
+</tbody>
+
+</table>
+
+
+
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
 
