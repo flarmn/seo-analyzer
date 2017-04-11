68457da38f1acd756b9f1e671adbdd01df5451df
Unknown <flarmn@gmail.com>
2017-04-11 15:07:19 +0300

    Checks: if file exist, server headers, remote file size

--- seo_analyzer.php
+++ seo_analyzer.php
@@ -7,7 +7,7 @@
 
 
 <?php
-$checkedAddres = $_POST["address"];
+$checkedAddress = $_POST["address"];
 // ===============
 /*
 $filename = '/path/to/foo.txt';
@@ -21,18 +21,40 @@ if (file_exists($filename)) {
 // ==================
 
 
-$robotspath= file_get_contents($checkedAddres . '/robots.txt');
-echo $robotspath;
+//$robotspath = file_get_contents($checkedAddress . '/robots.txt');
+
+$robotspath = fopen($checkedAddress . '/robots.txt', 'r');
+
+//echo $robotspath;
 
 if ($robotspath == false){
 echo 'No robots file';
 } else {
-	echo 'Waw!';
+	echo 'Robots file exist. All ok!';
 }
 
 
+$serverAnswer=(get_headers($checkedAddress)[0]);
+
+$robotsSize = (stream_get_meta_data($robotspath)['wrapper_data'][6]);
 
 
 
+echo '<br/>' . 'Server answer: ' . $serverAnswer . '<br/>';
+
+
+
+
+
+//print_r($robotsSize);
+
+$robotFSize = explode(':', $robotsSize);
+
+//echo $robotsSize;
+
+//print_r($robotFSize);
+
+echo 'File size: ' . $robotFSize[1] . " Bytes";
 
 ?>
+
