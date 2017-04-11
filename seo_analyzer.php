f5e4759360734e4be358a0aff29d13563618e690
Unknown <flarmn@gmail.com>
2017-04-11 19:46:21 +0300

    Engine ready

--- seo_analyzer.php
+++ seo_analyzer.php
@@ -8,7 +8,9 @@
 
 <?php
 $checkedAddress = $_POST["address"];
-// ===============
+
+
+// ====== IsExist? =======
 /*
 $filename = '/path/to/foo.txt';
 
@@ -36,25 +38,51 @@ echo 'No robots file';
 
 $serverAnswer=(get_headers($checkedAddress)[0]);
 
-$robotsSize = (stream_get_meta_data($robotspath)['wrapper_data'][6]);
 
+$robotsSize = (stream_get_meta_data($robotspath)['wrapper_data'][6]);
 
 
 echo '<br/>' . 'Server answer: ' . $serverAnswer . '<br/>';
 
 
-
-
-
-//print_r($robotsSize);
-
 $robotFSize = explode(':', $robotsSize);
 
-//echo $robotsSize;
-
-//print_r($robotFSize);
 
 echo 'File size: ' . $robotFSize[1] . " 	Bytes";
 
+
+/*
+if (substr_count($robotspath, 'H')>0){
+    echo "Найден!";
+}
+else{
+    echo "Не найден!";
+} 
+*/
+$robotssearch = file_get_contents($checkedAddress . '/robots.txt');
+//print_r($robotssearch);
+
+echo $robotssearch;
+
+$str="Hello";
+//$file=file("$checkedAddress . '/robots.txt");
+if(in_array($str,$robotssearch)){
+echo "Превед, медвед...";
+} 
+
+
+
+
+if (substr_count($robotssearch, 'Hello')>0){
+    echo "<br/>Найден! Искомое слово встречаеться " . substr_count($robotssearch, 'Hello') . ' раз';
+}
+else{
+    echo "Не найден!";
+} 
+
+//echo (substr_count($robotssearch, 'Hello'));
+
+
+//END 
 ?>
 
