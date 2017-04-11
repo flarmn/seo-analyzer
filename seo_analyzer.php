ebf3efb5899f1cae436c0de943bd8b10f9771433
Unknown <flarmn@gmail.com>
2017-04-11 21:58:05 +0300

    Engine update.
    Suits for all types of remote file meta data

--- seo_analyzer.php
+++ seo_analyzer.php
@@ -7,81 +7,57 @@
 
 
 <?php
+
+
 $checkedAddress = $_POST["address"];
 
 
-// ====== IsExist? =======
-/*
-$filename = '/path/to/foo.txt';
-
-if (file_exists($filename)) {
-    echo "Файл $filename существует";
-} else {
-    echo "Файл $filename не существует";
-}
-*/
-// ==================
-
-
-//$robotspath = file_get_contents($checkedAddress . '/robots.txt');
-
 $robotspath = fopen($checkedAddress . '/robots.txt', 'r');
 
-//echo $robotspath;
-
+// Checking if file exist at place
 if ($robotspath == false){
 echo 'No robots file';
 } else {
 	echo 'Robots file exist. All ok!';
 }
 
-
+// Getting server answer
 $serverAnswer=(get_headers($checkedAddress)[0]);
 
-
-$robotsSize = (stream_get_meta_data($robotspath)['wrapper_data'][6]);
-
-
-echo '<br/>' . 'Server answer: ' . $serverAnswer . '<br/>';
+echo '<br/>' . 'Server answer: ' . $serverAnswer ;
 
 
-$robotFSize = explode(':', $robotsSize);
+
+// GETTING REMOTE FILE SIZE FROM REMOTE file METADATA
+$remoteFileMeta =  (stream_get_meta_data($robotspath)['wrapper_data']);
+
+for ($i = 0; $i < count($remoteFileMeta); $i++){
+
+if(substr_count($remoteFileMeta[$i], 'Content-Length')>0){
+$robotFSize = explode(':', $remoteFileMeta[$i])[1];
+echo '<br/> File size: ' . $robotFSize . ' Bytes';
+}//if
+
+}//for 
 
 
-echo 'File size: ' . $robotFSize[1] . " 	Bytes";
 
+// Checking if Host appear in robots file
+$searchTerm  = "Host";
+$robotssearch = file_get_contents($checkedAddress . '/robots.txt');
+$termsCount = substr_count($robotssearch, $searchTerm);
+
+if (substr_count($robotssearch, $searchTerm)>0){
+    echo "<br/>Найден! Искомое слово встречаеться " . $termsCount . ' раз';
+}
+else{
+    echo "<br/>Не найден!";
+} 
 
 /*
-if (substr_count($robotspath, 'H')>0){
-    echo "Найден!";
-}
-else{
-    echo "Не найден!";
-} 
+echo '<br/>';
+print_r($remoteFileMeta);
 */
-$robotssearch = file_get_contents($checkedAddress . '/robots.txt');
-//print_r($robotssearch);
-
-echo $robotssearch;
-
-$str="Hello";
-//$file=file("$checkedAddress . '/robots.txt");
-if(in_array($str,$robotssearch)){
-echo "Превед, медвед...";
-} 
-
-
-
-
-if (substr_count($robotssearch, 'Hello')>0){
-    echo "<br/>Найден! Искомое слово встречаеться " . substr_count($robotssearch, 'Hello') . ' раз';
-}
-else{
-    echo "Не найден!";
-} 
-
-//echo (substr_count($robotssearch, 'Hello'));
-
 
 //END 
 ?>
