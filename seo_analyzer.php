2f2cf4b45994519896e7cb5303ea9a56607c8568
Unknown <flarmn@gmail.com>
2017-04-12 23:07:19 +0300

    quantity of host directives, filesize

--- seo_analyzer.php
+++ seo_analyzer.php
@@ -64,6 +64,16 @@ echo '<br/> File size: ' . $robotFSize . ' Bytes';
 
 }//for 
 
+if ($robotFSize > 32000){
+    $robotFSizeCountStatus = "Размера файла robots.txt составляет " . $robotFSize . " байт, что превышает допустимую норму";
+	$robotFSizeRecomendation = "Программист: Максимально допустимый размер файла robots.txt составляем 32 кб. Необходимо отредактировть файл robots.txt таким образом, чтобы его размер не превышал 32 Кб";
+$robotFSizeStatus = '<p style = "display:inline-block; height:100%; width:100%; background:red; color:white;">Oшибка</p>';
+} else{
+$robotFSizeCountStatus = "Размера файла robots.txt составляет " . $robotFSize . " байт, что находится в пределах допустимой нормы";
+$robotFSizeRecomendation = "Доработки не требуются";
+$robotFSizeStatus = '<p style = "display:inline-block; height:100%; width:100%; background:green; color:white;">OK</p>';
+}
+
 
 
 // Checking if Host appear in robots file
@@ -84,6 +94,24 @@ else{
 	$hostExistRecomendation = "Программист: Для того, чтобы поисковые системы знали, какая версия сайта является основных зеркалом, необходимо прописать адрес основного зеркала в директиве Host. В данный момент это не прописано. Необходимо добавить в файл robots.txt директиву Host. Директива Host задётся в файле 1 раз, после всех правил.";
 $hostStatus = '<p style = "display:inline-block; height:100%; width:100%; background:red; color:white;">Oшибка</p>';
 } 
+
+// Checking if Host appear more then once in robots files, and how much times
+
+if ($termsCount == 1){
+	$hostCountExistStatus = "В файле прописана " . $termsCount . " директива Host";
+	$hostCountRecomendation = "Доработки не требуются";
+	$hostCountStatus = '<p style = "display:inline-block; height:100%; width:100%; background:green; color:white;">Ok</p>';
+
+} else if ($termsCount > 1){
+
+$hostCountExistStatus = "В файле прописано " . $termsCount . " директив Host ";
+$hostCountRecomendation = "Программист: Директива Host должна быть указана в файле толоко 1 раз. Необходимо удалить все дополнительные директивы Host и оставить только 1, корректную и соответствующую основному зеркалу сайта";
+$hostCountStatus = '<p style = "display:inline-block; height:100%; width:100%; background:red; color:white;">Ошибка</p>';
+} else {
+	$hostCountExistStatus = "В файле прописано " . $termsCount . " директив Host ";
+$hostCountRecomendation = "Программист: Для того, чтобы поисковые системы знали, какая версия сайта является основных зеркалом, необходимо прописать адрес основного зеркала в директиве Host. В данный момент это не прописано. Необходимо добавить в файл robots.txt директиву Host. Директива Host задётся в файле 1 раз, после всех правил.";
+$hostCountStatus = '<p style = "display:inline-block; height:100%; width:100%; background:red; color:white;">Ошибка</p>';
+}
 
 
 
@@ -160,10 +188,12 @@ $hostStatus = '<p style = "display:inline-block; height:100%; width:100%; backgr
 	Проверка количества директив Host, прописанных в файле
 	</th>
 	<th>
-	Статус
+	<?php echo $hostCountStatus; ?>
 	</th>
 	<th>
-	Текущее состояние
+	<?php echo 'Состояние: ' . $hostCountExistStatus . "<br/>"; ?>
+	<hr>
+	<?php echo 'Рекомендации: ' . $hostCountRecomendation . "<br/>"; ?>
 	</th>
 </tr>
 
@@ -176,10 +206,12 @@ $hostStatus = '<p style = "display:inline-block; height:100%; width:100%; backgr
 	Проверка размера файла robots.txt
 	</th>
 	<th>
-	Статус
+	<?php echo $robotFSizeStatus; ?>
 	</th>
 	<th>
-	Текущее состояние
+		<?php echo 'Состояние: ' . $robotFSizeCountStatus . "<br/>"; ?>
+	<hr>
+	<?php echo 'Рекомендации: ' . $robotFSizeRecomendation . "<br/>"; ?>
 	</th>
 </tr>
 
