b44df9adaf636041395d5823ac12e1598282ce11
Unknown <flarmn@gmail.com>
2017-04-27 20:36:05 +0300

    oop5

--- seo_analyzer_oop.php
+++ seo_analyzer_oop.php
@@ -28,12 +28,21 @@
 
 class seo_tester{
 
+public $checkedAddress = 'http://fairodis-jewelry.fairiesgifts.net/';
 
 
+function check_robots_file(){
+$this->robotspath = fopen($this->checkedAddress . '/robots.txt', 'r');
 
-public	function seo_prober(){
+// Checking if file exist at place
+if ($this->robotspath == false){
+echo 'No robots file';
+} else {
+	echo 'Robots file exist. All ok!';
+}//else
 
-}
+
+}//func
 
 
 
@@ -60,7 +69,9 @@ function seo_error_messages(){
 
 
 
+$seotester = new seo_tester();
 
+$seotester->check_robots_file();
 
 
 
--- test_modules/if_robots_exist.php
+++ test_modules/if_robots_exist.php
@@ -8,39 +8,24 @@ class if_robots_exist{
 
 	public $checkedAddress = 'http://fairodis-jewelry.fairiesgifts.net/';
 
-	function check_robots_file(){
-		//$checkedAddress = $_POST["address"];
-
-		
-
-		//$checkedAddress = 'http://fairodis-jewelry.fairiesgifts.ne/';
-
-
+function check_robots_file(){
 $this->robotspath = fopen($this->checkedAddress . '/robots.txt', 'r');
 
 // Checking if file exist at place
 if ($this->robotspath == false){
 echo 'No robots file';
-//$this->robotsExistStatus = "Файл robots.txt отсутствует";
-	//$this->robotsExistRecomendation = "Программист: Создать файл robots.txt и разместить его на сайте.";
-	//$this->robotsStatus = '<p style = "display:inline-block; background:red; color:white;">Ошибка</p>';
-
 } else {
 	echo 'Robots file exist. All ok!';
-	//$robotsExistStatus = "Файл robots.txt присутствует";
-	//$robotsExistRecomendation = "Доработки не требуются";
-	//$robotsStatus = '<p style = "display:inline-block; height:100%; width:100%; background:green; color:white;">Ok</p>';
+}//else
 
-}
-	}
-}
+
+	}//function
+}//class
 
 
 $if_robots_exist = new if_robots_exist();
 
 $if_robots_exist->check_robots_file();
-
-
 
 //END 
 ?>
